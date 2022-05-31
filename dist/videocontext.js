fc290e53cfa08839b2d2784134ce5b6478522368
changx <changx@gmail.com>
2022-05-31 17:01:23 +0800

    rebuild

--- dist/videocontext.js
+++ dist/videocontext.js
@@ -4606,11 +4606,14 @@ function createElementTexture(gl) {
 
 function updateTexture(gl, texture, element) {
     if (element.readyState !== undefined && element.readyState === 0) return;
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, element);
 
-    texture._isTextureCleared = false;
+    if (element.readyState === 4 && (element.webkitDecodedFrameCount || element.mozDecodedFrames)) {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, element);
+
+        texture._isTextureCleared = false;
+    }
 }
 
 function clearTexture(gl, texture) {
